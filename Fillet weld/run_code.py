4ed9a1ff1162acc248dbfc03b82dd1a5468e7ce4
GustavHultgrenKTH <91541954+GustavHultgrenKTH@users.noreply.github.com>
2026-03-31 12:33:59 +0200

    Changed name on Python function
    Changed name of the python function in the input file

--- Fillet weld/run_code.py
+++ Fillet weld/run_code.py
@@ -6,7 +6,7 @@ weld_file  = os.path.join(base, "V5_5_2_cs_cs.pcd")  # or .mat
 
 bw = filletweld3d.initialize()
 try:
-    inputFile = bw.fun_buttweld_3D(param_file, weld_file, "V5_5_2_cs_cs", nargout=1)
+    inputFile = bw.weld_3D(param_file, weld_file, "V5_5_2_cs_cs", nargout=1)
     print("inputFile:", inputFile)
     #print("nodeFile:", nodeFile)
     #print("elemFile:", elemFile)
